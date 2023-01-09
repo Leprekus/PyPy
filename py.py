5a30e463d7061910cb2e1b2a090b5beac51dfb07
leprekus <leprekus@yahoo.com>
2023-01-09 09:24:26 -0800

    initial commit

--- py.py
+++ py.py
@@ -1,11 +1,23 @@
-from PyQt5.QtWidgets import QApplication, QWidget
 import sys
+from PyQt5.QtWidgets import ( 
+    QApplication, 
+    QMainWindow,
+    QPushButton
+    )
+
+class MainWindow(QMainWindow):
+    #avoids referring to main class
+    #helps with inheritance 
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('My App')
+        button = QPushButton('Press Me')
+        self.setCentralWidget(button)
 
 #if no cli arguments will be used an empty list can be pased
 app = QApplication(sys.argv)
-
-window = QWidget()
+window = MainWindow()
 window.show()
 
 app.exec_()
-print(sys.version)
